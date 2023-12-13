2eeb0dc9467c3b6a262c6db6764c4fb43af5ca91
Neibagha Kingsway ThankGod <115288486+ItsThankK@users.noreply.github.com>
2023-12-13 17:18:40 +0100

    create addNumber.sol

---
+++ challenge-3/addNumber.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract AddString {
+
+    function addNumbers(uint _num1, uint _num2) public pure returns (uint) {
+        uint sum = _num1 + _num2;
+       return sum;
+    }
+}
