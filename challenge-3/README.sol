f0eb4a12473c3c4e434718511a6282097c57924e
Neibagha Kingsway ThankGod <115288486+ItsThankK@users.noreply.github.com>
2023-12-13 17:27:38 +0100

    create README.md

---
+++ challenge-3/README.sol
@@ -0,0 +1,8 @@
+# challenge 3
+
+- Contract was written and deployed in Remix IDE
+- Deployed to Sepolia testnet
+
+- Contract address: 0x67DCF05DEe67423e0747c6dE302E31C67C1f5d79
+- Contract on Sepolia explorer: https://sepolia.etherscan.io/address/0x67dcf05dee67423e0747c6de302e31c67c1f5d79
+- Deployment of the contract on Sepolia: https://sepolia.etherscan.io/tx/0xab81897b08584bdbce9999aa31f9753c13466937d5ef4a6794f25e595cb1f855
